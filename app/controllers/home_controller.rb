9f59a90300a0eed522495b0a32ba96a202f33841
Anshuman Prusty <anshuman.prusty@gmail.com>
2016-11-30 19:25:43 +0530

    filters added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,20 @@ class HomeController < ApplicationController
   def index
   	$a = 0
   	@agencies = Agency.all
-    @packages = Package.all  	
+    
+    @prices = Package.select(:price).distinct
+
+    @durations = Package.select(:duration).distinct
+
+    if params[:agency_id]
+    	@packages = Package.where(agency_id: params[:agency_id])
+    elsif params[:price]
+    	@packages = Package.where(price: params[:price])
+    elsif params[:duration]
+    	@packages = Package.where(duration: params[:duration])
+    else
+    	@packages = Package.all
+    end    
+
   end
 end
